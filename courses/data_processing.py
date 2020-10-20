3f4b329f8c6eb722a0fd671564f20cfd4406d073
Julien Bordas <julien.bordas@amadeus.com>
2020-10-20 17:10:24 +0200

    Concat num and categorical values

--- courses/data_processing.py
+++ courses/data_processing.py
@@ -1,41 +1,92 @@
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
 
 
-def process_missing_values(train_X, val_X, test_X):
+def check_data(train_X, val_X, test_X):
     """
-    Use imputation to handle missing value, fit the transform on training data and apply it as well on validation and test
+    Perform checks on the preprocessed data
+    :param train_X: processed training data
+    :param val_X: processed validation data
+    :param test_X: processed testing data
+    """
+    # Assert there is no missing values anymore
+    assert train_X.isnull().sum()[train_X.isnull().sum() > 0].empty
+    assert val_X.isnull().sum()[val_X.isnull().sum() > 0].empty
+    assert test_X.isnull().sum()[test_X.isnull().sum() > 0].empty
+
+    # Assert there are only numerical values left
+    assert train_X.select_dtypes(include=['object']).empty
+    assert val_X.select_dtypes(include=['object']).empty
+    assert test_X.select_dtypes(include=['object']).empty
+
+
+def process_categorical_values(train_X, val_X, test_X, cat_cols):
+    """
+    Process categorical missing values and convert them to numerical
+    Use label encoding and one hot encoding to handle categorical values
     :param train_X: training data
     :param val_X: validation data
     :param test_X: testing data
-    :return: processed_train_X, processed_val_X, processed_test_X
+    :param cat_cols: columns containing categorical values only
+    :return: processed_cat_train_X, processed_cat_val_X, processed_cat_test_X
+    """
+    cat_train_X = train_X[cat_cols]
+    processed_cat_train_X = cat_train_X.copy()
+    for col in cat_train_X.columns:
+        processed_cat_train_X[col].values[:] = 0
+    processed_cat_train_X.index = train_X.index
+    processed_cat_train_X = processed_cat_train_X.astype('int64')
+
+    cat_val_X = val_X[cat_cols]
+    processed_cat_val_X = cat_val_X.copy()
+    for col in cat_val_X.columns:
+        processed_cat_val_X[col].values[:] = 0
+    processed_cat_val_X.index = val_X.index
+    processed_cat_val_X = processed_cat_val_X.astype('int64')
+
+    cat_test_X = test_X[cat_cols]
+    processed_cat_test_X = cat_test_X.copy()
+    for col in cat_test_X.columns:
+        processed_cat_test_X[col].values[:] = 0
+    processed_cat_test_X.index = test_X.index
+    processed_cat_test_X = processed_cat_test_X.astype('int64')
+
+    check_data(processed_cat_train_X, processed_cat_val_X, processed_cat_test_X)
+    return processed_cat_train_X, processed_cat_val_X, processed_cat_test_X
+
+
+def process_numerical_values(train_X, val_X, test_X, num_cols):
+    """
+    Process numerical missing values
+    Use imputation to handle missing value, fit the transform on training data and apply it on validation and test
+    :param train_X: training data
+    :param val_X: validation data
+    :param test_X: testing data
+    :param num_cols: columns containing numerical values only
+    :return: processed_num_train_X, processed_num_val_X, processed_num_test_X
     """
     imputer = SimpleImputer(strategy='median')
 
-    processed_train_X = pd.DataFrame(imputer.fit_transform(train_X))
-    processed_train_X.columns = train_X.columns
+    num_train_X = train_X[num_cols]
+    processed_num_train_X = pd.DataFrame(imputer.fit_transform(num_train_X))
+    processed_num_train_X.columns = num_train_X.columns
+    processed_num_train_X.index = train_X.index
 
-    processed_val_X = pd.DataFrame(imputer.transform(val_X))
-    processed_val_X.columns = val_X.columns
+    num_val_X = val_X[num_cols]
+    processed_num_val_X = pd.DataFrame(imputer.transform(num_val_X))
+    processed_num_val_X.columns = num_val_X.columns
+    processed_num_val_X.index = val_X.index
 
-    processed_test_X = pd.DataFrame(imputer.transform(test_X))
-    processed_test_X.columns = test_X.columns
+    num_test_X = test_X[num_cols]
+    processed_num_test_X = pd.DataFrame(imputer.transform(num_test_X))
+    processed_num_test_X.columns = num_test_X.columns
+    processed_num_test_X.index = test_X.index
 
-    return processed_train_X, processed_val_X, processed_test_X
-
-
-def process_categorical_values(train_X, val_X, test_X):
-    """
-    Use imputation to handle missing value, fit the transform on training data and apply it as well on the validation
-    :param train_X: training data
-    :param val_X: validation data
-    :return: processed_train_X, processed_val_X
-    """
-
-
-    return processed_train_X, processed_val_X, processed_test_X
+    check_data(processed_num_train_X, processed_num_val_X, processed_num_test_X)
+    return processed_num_train_X, processed_num_val_X, processed_num_test_X
 
 
 def preprocess(train_file, test_file, target, index, ratio):
@@ -62,10 +113,18 @@ def preprocess(train_file, test_file, target, index, ratio):
     X = train_data.drop([target], axis=1)
 
     # Split training data into training and validation using [ratio]
-    train_X, val_X, train_y, val_y = train_test_split(X, y, train_size=ratio, test_size=1-ratio)
+    train_X, val_X, train_y, val_y = train_test_split(X, y, train_size=ratio, test_size=1 - ratio)
 
     # Process the data
-    train_X, val_X, test_X = process_missing_values(train_X, val_X, test_X)
-    train_X, val_X, test_X = process_categorical_values(train_X, val_X, test_X)
+    num_cols = [col for col in train_X.columns if train_X[col].dtype != 'object']
+    cat_cols = [col for col in train_X.columns if train_X[col].dtype == 'object']
+    assert (len(num_cols) + len(cat_cols)) == train_X.shape[1]
 
-    return train_X, val_X, train_y, val_y, test_X
+    num_train_X, num_val_X, num_test_X = process_numerical_values(train_X, val_X, test_X, num_cols)
+    cat_train_X, cat_val_X, cat_test_X = process_categorical_values(train_X, val_X, test_X, cat_cols)
+
+    return num_train_X.join(cat_train_X.set_index(train_X.index), on=train_X.index), \
+           num_val_X.join(cat_val_X.set_index(val_X.index), on=val_X.index), \
+           train_y, \
+           val_y, \
+           num_test_X.join(cat_test_X.set_index(test_X.index), on=test_X.index)
